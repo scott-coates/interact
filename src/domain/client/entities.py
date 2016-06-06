e5ca6c96f634589376d0990ac14462736db53c81
scott coates <scoarescoare@gmail.com>
2016-06-06 13:35:48 -0500

    adding TA topics

--- src/domain/client/entities.py
+++ src/domain/client/entities.py
@@ -24,3 +24,45 @@ class Client(AggregateBase):
 
   def __str__(self):
     return 'Client {id}: {name}'.format(id=self.id, name=self.name)
+
+
+class TargetAudienceTopic:
+  def __init__(self, id, topic_id, system_created_date):
+
+    if not id:
+      raise TypeError("id is required")
+
+    if not topic_id:
+      raise TypeError("topic_id is required")
+
+    self.id = id
+    self.topic_id = topic_id
+    self.system_created_date = system_created_date
+
+  def __str__(self):
+    return 'TATopic {id}'.format(id=self.id)
+
+
+class TargetAudienceTopicOption:
+  def __init__(self, id, category_type, attrs, ta_topic_id, system_created_date):
+
+    if not id:
+      raise TypeError("id is required")
+
+    if not ta_topic_id:
+      raise TypeError("ta_topic_id is required")
+
+    if not category_type:
+      raise TypeError("category_type is required")
+
+    if not attrs:
+      raise TypeError("attrs is required")
+
+    self.id = id
+    self.category_type = category_type
+    self.attrs = attrs
+    self.ta_topic_id = ta_topic_id
+    self.system_created_date = system_created_date
+
+  def __str__(self):
+    return 'TATopicOption{id}'.format(id=self.id)
