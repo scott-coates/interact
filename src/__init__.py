84e194a12ddd22f413c817fd80b1a0386bbcc9b2
scott coates <scoarescoare@gmail.com>
2016-06-07 11:55:37 -0500

    todo

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-import nltk
-
-# http://stackoverflow.com/questions/13965823/resource-corpora-wordnet-not-found-on-heroku
-nltk.data.path.append('./src/nltk_data')
+# todo why does importing nltk cause http and network requests to hang during rq tasks?
+# import nltk
+#
+# # http://stackoverflow.com/questions/13965823/resource-corpora-wordnet-not-found-on-heroku
+# nltk.data.path.append('./src/nltk_data')
