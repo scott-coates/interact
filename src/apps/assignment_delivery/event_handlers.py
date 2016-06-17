368cb48b876d9db6537c1c88be61eb58356c244b
scott coates <scoarescoare@gmail.com>
2016-06-17 12:06:44 -0500

    not idempotent

--- src/apps/assignment_delivery/event_handlers.py
+++ src/apps/assignment_delivery/event_handlers.py
@@ -2,10 +2,10 @@ from django.dispatch import receiver
 
 from src.apps.assignment_delivery import tasks
 from src.domain.client.events import ClientAddedEngagementAssignment1
-from src.libs.common_domain.decorators import event_idempotent
+# from src.libs.common_domain.decorators import event_idempotent
 
 
-@event_idempotent
+# @event_idempotent
 @receiver(ClientAddedEngagementAssignment1.event_signal)
 def execute_ea_created_1(**kwargs):
   event = kwargs['event']
