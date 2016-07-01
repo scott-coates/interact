46479ccbe7632fc3ecbee290276dcfce9eb86ee0
scott coates <scoarescoare@gmail.com>
2016-07-01 15:43:15 -0500

    rqscheduler

--- src/apps/engagement_discovery/event_handlers.py
+++ src/apps/engagement_discovery/event_handlers.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+import django_rq
 from django.dispatch import receiver
 
 from src.apps.engagement_discovery import tasks
@@ -6,10 +9,15 @@ from src.domain.prospect.events import ProspectAddedProfile1
 
 @receiver(ProspectAddedProfile1.event_signal)
 def execute_prospect_added_profile_1(**kwargs):
+  scheduler = django_rq.get_scheduler('default')
+
   prospect_id = kwargs['aggregate_id']
 
   event = kwargs['event']
+
   external_id = event.external_id
   provider_type = event.provider_type
 
-  tasks.discover_engagement_opportunities_from_profile_task.delay(external_id, provider_type, prospect_id)
+  # delay this task so that we have time to check if it's a duplicate prospect before we proceed
+  scheduler.enqueue_in(timedelta(minutes=5), tasks.discover_engagement_opportunities_from_profile_task,
+                       external_id, provider_type, prospect_id)
