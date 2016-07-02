e9a403bcb241a1f3ece76f668fa721441f30cfcd
scott coates <scoarescoare@gmail.com>
2016-07-01 20:35:58 -0500

    remove

--- src/apps/engagement_discovery/event_handlers.py
+++ src/apps/engagement_discovery/event_handlers.py
@@ -9,7 +9,6 @@ from src.domain.prospect.events import ProspectAddedProfile1
 
 @receiver(ProspectAddedProfile1.event_signal)
 def execute_prospect_added_profile_1(**kwargs):
-  print('*****scheduler********')
   scheduler = django_rq.get_scheduler('default')
 
   prospect_id = kwargs['aggregate_id']
