ae97af8ea0c76cbdd105725c9709b6809583954e
scott coates <scoarescoare@gmail.com>
2016-07-15 13:48:16 -0500

    record twitter api call

--- src/libs/social_utils/providers/twitter/twitter_client_service.py
+++ src/libs/social_utils/providers/twitter/twitter_client_service.py
@@ -1,4 +1,5 @@
 from itertools import chain
+
 from src.libs.python_utils.collections.iter_utils import chunks
 from src.libs.social_utils.providers.twitter import twitter_search_utils
 
@@ -36,8 +37,3 @@ def _search_chunks(*query_items, action, _search=twitter_search_utils, **kwargs)
   ret_val = [_search.search_twitter(query, **kwargs) for query in query_chunks]
 
   return chain.from_iterable(x['statuses'] for x in ret_val)
-
-
-def get_twitter_external_id(url):
-  # for now, just assume it's a normal twitter url - i.e. it doesn't contain a status
-  return url.rsplit('/', 1)[-1]
