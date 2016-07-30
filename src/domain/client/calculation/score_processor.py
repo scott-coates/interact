46d0a8025ccde012595d1b1edbecf22c16c157d6
scott coates <scoarescoare@gmail.com>
2016-07-30 15:35:53 -0500

    revert

--- src/domain/client/calculation/score_processor.py
+++ src/domain/client/calculation/score_processor.py
@@ -5,15 +5,15 @@ from src.domain.common import constants
 
 def process_score(score_attrs):
   score = score_attrs[constants.PROSPECT][constants.SCORE] + \
-          sum([x[constants.SCORE] for x in score_attrs[constants.PROFILES]]) + \
-          sum([x[constants.SCORE] for x in score_attrs[constants.ASSIGNED_ENTITIES]])
+          max([x[constants.SCORE] for x in score_attrs[constants.PROFILES]]) + \
+          max([x[constants.SCORE] for x in score_attrs[constants.ASSIGNED_ENTITIES]])
 
   return score, score_attrs
 
 
 def _get_score(score):
   try:
-    ret_val = log(max(score, 1), 10)
+    ret_val = log(max(score, 1.1), 10)
   except Exception as e:
     raise Exception('Error', score).with_traceback(e.__traceback__)
   return ret_val
