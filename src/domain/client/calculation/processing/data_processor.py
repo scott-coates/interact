acb0ea8757436e3036e04bb54955768d186c0285
scott coates <scoarescoare@gmail.com>
2016-08-14 01:58:39 -0500

    change compare text

--- src/domain/client/calculation/processing/data_processor.py
+++ src/domain/client/calculation/processing/data_processor.py
@@ -1,25 +1,46 @@
 from nltk import TweetTokenizer
+from nltk.corpus import stopwords as sp
 
 from src.libs.text_utils.formatting.text_formatter import only_alpha_numeric
+from src.libs.text_utils.token.token_utils import find_stem
 
-invalid_tweet_tokens = ('http', '#', '@')
+invalid_tweet_token_prefix = ('http', '#', '@')
 tt = TweetTokenizer()
+
+stopwords = sp.words('english')
 
 
 def get_comparative_text_from_tweet(tweet):
   text_tokens = tt.tokenize(tweet)
 
-  comparative_text = filter(_valid_tweet_token, text_tokens)
-  comparative_text = [only_alpha_numeric(x).lower() for x in comparative_text]
+  comparative_text = filter(_valid_tweet_token_prefix, text_tokens)
+  comparative_text = map(_normalize_tokens, comparative_text)
+  comparative_text = (only_alpha_numeric(x).lower() for x in comparative_text)
+  comparative_text = filter(_valid_tweet_token, comparative_text)
   comparative_text = list(filter(bool, comparative_text))
 
   return comparative_text
 
 
+def _valid_tweet_token_prefix(token):
+  ret_val = True
+
+  if any(it for it in invalid_tweet_token_prefix if token.startswith(it)):
+    ret_val = False
+
+  return ret_val
+
+
 def _valid_tweet_token(token):
   ret_val = True
 
-  if any(it for it in invalid_tweet_tokens if token.startswith(it)):
+  if token in stopwords:
     ret_val = False
+
+  return ret_val
+
+
+def _normalize_tokens(token):
+  ret_val = find_stem(token)
 
   return ret_val
